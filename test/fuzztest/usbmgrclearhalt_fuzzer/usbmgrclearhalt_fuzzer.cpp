7f49961e5fa480862f05b858cafe7d1dbe409f8e
h60106262 <huangmengcheng@h-partners.com>
2026-06-01 15:49:28 +0800

    修复data参数未调用问题
    Signed-off-by: h60106262 <huangmengcheng@h-partners.com>

--- test/fuzztest/usbmgrclearhalt_fuzzer/usbmgrclearhalt_fuzzer.cpp
+++ test/fuzztest/usbmgrclearhalt_fuzzer/usbmgrclearhalt_fuzzer.cpp
@@ -24,6 +24,15 @@ constexpr size_t THRESHOLD = 10;
 namespace USB {
     bool UsbMgrClearHaltFuzzTest(const uint8_t* data, size_t /* size */)
     {
+        unsigned seed = 0;
+        if (size >= sizeof(unsigned)) {
+            errno_t ret = memcpy_s(&seed, sizeof(unsigned), data, sizeof(unsigned));
+            if (ret != UEC_OK) {
+                return false;
+            }
+            srand(seed);
+        }
+
         std::vector<UsbDevice> devList;
         auto &usbSrvClient = UsbSrvClient::GetInstance();
         auto ret = usbSrvClient.GetDevices(devList);
