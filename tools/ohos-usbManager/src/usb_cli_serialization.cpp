8f00572afa6cef1b5a97171f7885f19876a95e5d
liuchunyu20 <liuchunyu20@huawei.com>
2026-06-08 18:59:47 +0800

    add cli tool for usbManager
    Signed-off-by: liuchunyu20 <liuchunyu20@huawei.com>

---
+++ tools/ohos-usbManager/src/usb_cli_serialization.cpp
@@ -0,0 +1,214 @@
+/*
+ * Copyright (c) 2026 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "usb_cli_serialization.h"
+#include <sstream>
+#include "cJSON.h"
+#include "usb_config.h"
+#include "usb_endpoint.h"
+#include "usb_interface.h"
+
+namespace OHOS {
+namespace USB {
+
+static cJSON* SerializeEndpoint(const USBEndpoint &endpoint)
+{
+    cJSON *json = cJSON_CreateObject();
+    if (!json) {
+        return nullptr;
+    }
+    cJSON_AddNumberToObject(json, "address", static_cast<double>(endpoint.GetAddress()));
+    cJSON_AddNumberToObject(json, "attributes", static_cast<double>(endpoint.GetAttributes()));
+    cJSON_AddNumberToObject(json, "interval", static_cast<double>(endpoint.GetInterval()));
+    cJSON_AddNumberToObject(json, "maxPacketSize", static_cast<double>(endpoint.GetMaxPacketSize()));
+    cJSON_AddNumberToObject(json, "direction", static_cast<double>(endpoint.GetDirection()));
+    cJSON_AddNumberToObject(json, "number", static_cast<double>(endpoint.GetEndpointNumber()));
+    cJSON_AddNumberToObject(json, "type", static_cast<double>(endpoint.GetType()));
+    cJSON_AddNumberToObject(json, "interfaceId", static_cast<double>(endpoint.GetInterfaceId()));
+    return json;
+}
+
+static cJSON* SerializeInterface(const UsbInterface &iface)
+{
+    cJSON *json = cJSON_CreateObject();
+    if (!json) {
+        return nullptr;
+    }
+    cJSON_AddNumberToObject(json, "id", static_cast<double>(iface.GetId()));
+    cJSON_AddNumberToObject(json, "protocol", static_cast<double>(iface.GetProtocol()));
+    cJSON_AddNumberToObject(json, "clazz", static_cast<double>(iface.GetClass()));
+    cJSON_AddNumberToObject(json, "subClass", static_cast<double>(iface.GetSubClass()));
+    cJSON_AddNumberToObject(json, "alternateSetting", static_cast<double>(iface.GetAlternateSetting()));
+    cJSON_AddStringToObject(json, "name", iface.GetName().c_str());
+
+    cJSON *endpoints = cJSON_CreateArray();
+    if (!endpoints) {
+        cJSON_Delete(json);
+        return nullptr;
+    }
+    const auto &eps = iface.GetEndpoints();
+    for (size_t i = 0; i < eps.size(); ++i) {
+        cJSON *ep = SerializeEndpoint(eps[i]);
+        if (ep) {
+            cJSON_AddItemToArray(endpoints, ep);
+        }
+    }
+    cJSON_AddItemToObject(json, "endpoints", endpoints);
+    return json;
+}
+
+static cJSON* SerializeConfig(const USBConfig &config)
+{
+    cJSON *json = cJSON_CreateObject();
+    if (!json) {
+        return nullptr;
+    }
+    cJSON_AddNumberToObject(json, "id", static_cast<double>(config.GetId()));
+    cJSON_AddNumberToObject(json, "attributes", static_cast<double>(config.GetAttributes()));
+    cJSON_AddNumberToObject(json, "maxPower", static_cast<double>(config.GetMaxPower()));
+    cJSON_AddStringToObject(json, "name", config.GetName().c_str());
+    cJSON_AddBoolToObject(json, "isRemoteWakeup", config.IsRemoteWakeup());
+    cJSON_AddBoolToObject(json, "isSelfPowered", config.IsSelfPowered());
+
+    cJSON *interfaces = cJSON_CreateArray();
+    if (!interfaces) {
+        cJSON_Delete(json);
+        return nullptr;
+    }
+    for (uint32_t i = 0; i < config.GetInterfaceCount(); ++i) {
+        UsbInterface iface;
+        if (config.GetInterface(i, iface)) {
+            cJSON *intf = SerializeInterface(iface);
+            if (intf) {
+                cJSON_AddItemToArray(interfaces, intf);
+            }
+        }
+    }
+    cJSON_AddItemToObject(json, "interfaces", interfaces);
+    return json;
+}
+
+cJSON* SerializeDevice(const UsbDevice &device)
+{
+    cJSON *json = cJSON_CreateObject();
+    if (!json) {
+        return nullptr;
+    }
+    cJSON_AddNumberToObject(json, "busNum", static_cast<double>(device.GetBusNum()));
+    cJSON_AddNumberToObject(json, "devAddress", static_cast<double>(device.GetDevAddr()));
+    cJSON_AddStringToObject(json, "serial", device.GetmSerial().c_str());
+    cJSON_AddStringToObject(json, "name", device.GetName().c_str());
+    cJSON_AddStringToObject(json, "manufacturerName", device.GetManufacturerName().c_str());
+    cJSON_AddStringToObject(json, "productName", device.GetProductName().c_str());
+    cJSON_AddStringToObject(json, "version", device.GetVersion().c_str());
+    cJSON_AddNumberToObject(json, "vendorId", static_cast<double>(device.GetVendorId()));
+    cJSON_AddNumberToObject(json, "productId", static_cast<double>(device.GetProductId()));
+    cJSON_AddNumberToObject(json, "clazz", static_cast<double>(device.GetClass()));
+    cJSON_AddNumberToObject(json, "subClass", static_cast<double>(device.GetSubclass()));
+    cJSON_AddNumberToObject(json, "protocol", static_cast<double>(device.GetProtocol()));
+
+    cJSON *configs = cJSON_CreateArray();
+    if (!configs) {
+        cJSON_Delete(json);
+        return nullptr;
+    }
+    const auto &cfgs = device.GetConfigs();
+    for (size_t i = 0; i < cfgs.size(); ++i) {
+        cJSON *cfg = SerializeConfig(cfgs[i]);
+        if (cfg) {
+            cJSON_AddItemToArray(configs, cfg);
+        }
+    }
+    cJSON_AddItemToObject(json, "configs", configs);
+    return json;
+}
+
+cJSON* SerializeAccessory(const USBAccessory &accessory)
+{
+    cJSON *json = cJSON_CreateObject();
+    if (!json) {
+        return nullptr;
+    }
+    cJSON_AddStringToObject(json, "manufacturer", accessory.GetManufacturer().c_str());
+    cJSON_AddStringToObject(json, "product", accessory.GetProduct().c_str());
+    cJSON_AddStringToObject(json, "description", accessory.GetDescription().c_str());
+    cJSON_AddStringToObject(json, "version", accessory.GetVersion().c_str());
+    cJSON_AddStringToObject(json, "serialNumber", accessory.GetSerialNumber().c_str());
+    return json;
+}
+
+cJSON* SerializeSerialPort(const UsbSerialPort &port)
+{
+    cJSON *json = cJSON_CreateObject();
+    if (!json) {
+        return nullptr;
+    }
+    cJSON_AddNumberToObject(json, "portId", static_cast<double>(port.portId_));
+    std::ostringstream deviceName;
+    deviceName << static_cast<int>(port.busNum_) << "-" << static_cast<int>(port.devAddr_);
+    cJSON_AddStringToObject(json, "deviceName", deviceName.str().c_str());
+    return json;
+}
+
+std::string FormatSuccess(const std::vector<cJSON*> &items)
+{
+    cJSON *root = cJSON_CreateObject();
+    if (!root) {
+        return "{\"number\":0,\"list\":[]}";
+    }
+    cJSON *list = cJSON_CreateArray();
+    if (!list) {
+        cJSON_Delete(root);
+        return "{\"number\":0,\"list\":[]}";
+    }
+    for (auto *item : items) {
+        if (item) {
+            cJSON_AddItemToArray(list, item);
+        }
+    }
+    cJSON_AddNumberToObject(root, "number", static_cast<double>(cJSON_GetArraySize(list)));
+    cJSON_AddItemToObject(root, "list", list);
+
+    char *output = cJSON_PrintUnformatted(root);
+    cJSON_Delete(root);
+    if (!output) {
+        return "{\"number\":0,\"list\":[]}";
+    }
+    std::string result(output);
+    cJSON_free(output);
+    return result;
+}
+
+std::string FormatError(int32_t code, const std::string &message)
+{
+    cJSON *root = cJSON_CreateObject();
+    if (!root) {
+        return "{\"code\":-1,\"message\":\"internal error\"}";
+    }
+    cJSON_AddNumberToObject(root, "code", static_cast<double>(code));
+    cJSON_AddStringToObject(root, "message", message.c_str());
+
+    char *output = cJSON_PrintUnformatted(root);
+    cJSON_Delete(root);
+    if (!output) {
+        return "{\"code\":-1,\"message\":\"internal error\"}";
+    }
+    std::string result(output);
+    cJSON_free(output);
+    return result;
+}
+
+}
+}
---
+++ tools/ohos-usbManager/tests/usb_cli_serialization_test.cpp
@@ -0,0 +1,229 @@
+/*
+ * Copyright (c) 2026 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <gtest/gtest.h>
+#include <string>
+#include <vector>
+#include "cJSON.h"
+#include "usb_cli_serialization.h"
+#include "usb_device.h"
+#include "usb_accessory.h"
+#include "usb_serial_type.h"
+
+using namespace OHOS::USB;
+using namespace testing::ext;
+
+class UsbCliSerializationTest : public testing::Test {};
+
+HWTEST_F(UsbCliSerializationTest, SerializeDevice_PublicApiFields, TestSize.Level1)
+{
+    std::vector<USBConfig> configs;
+    UsbDevice device("test_device", "test_mfg", "test_product", "1.0",
+        2, 1, 0x1234, 0x5678, 0, 0, 0, configs);
+    device.SetmSerial("SN12345");
+
+    cJSON *json = SerializeDevice(device);
+    ASSERT_NE(json, nullptr);
+
+    cJSON *item = cJSON_GetObjectItem(json, "busNum");
+    ASSERT_NE(item, nullptr);
+    EXPECT_EQ(item->valueint, 1);
+
+    item = cJSON_GetObjectItem(json, "devAddress");
+    ASSERT_NE(item, nullptr);
+    EXPECT_EQ(item->valueint, 2);
+
+    item = cJSON_GetObjectItem(json, "serial");
+    ASSERT_NE(item, nullptr);
+    EXPECT_STREQ(item->valuestring, "SN12345");
+
+    item = cJSON_GetObjectItem(json, "name");
+    ASSERT_NE(item, nullptr);
+    EXPECT_STREQ(item->valuestring, "test_device");
+
+    item = cJSON_GetObjectItem(json, "manufacturerName");
+    ASSERT_NE(item, nullptr);
+    EXPECT_STREQ(item->valuestring, "test_mfg");
+
+    item = cJSON_GetObjectItem(json, "productName");
+    ASSERT_NE(item, nullptr);
+    EXPECT_STREQ(item->valuestring, "test_product");
+
+    item = cJSON_GetObjectItem(json, "vendorId");
+    ASSERT_NE(item, nullptr);
+    EXPECT_EQ(item->valueint, 0x1234);
+
+    item = cJSON_GetObjectItem(json, "productId");
+    ASSERT_NE(item, nullptr);
+    EXPECT_EQ(item->valueint, 0x5678);
+
+    item = cJSON_GetObjectItem(json, "clazz");
+    ASSERT_NE(item, nullptr);
+
+    item = cJSON_GetObjectItem(json, "subClass");
+    ASSERT_NE(item, nullptr);
+
+    item = cJSON_GetObjectItem(json, "protocol");
+    ASSERT_NE(item, nullptr);
+
+    item = cJSON_GetObjectItem(json, "configs");
+    ASSERT_NE(item, nullptr);
+    EXPECT_TRUE(cJSON_IsArray(item));
+
+    cJSON_Delete(json);
+}
+
+HWTEST_F(UsbCliSerializationTest, SerializeDevice_NoInternalFields, TestSize.Level1)
+{
+    std::vector<USBConfig> configs;
+    UsbDevice device("name", "mfg", "prod", "1.0", 1, 1, 0, 0, 0, 0, 0, configs);
+
+    cJSON *json = SerializeDevice(device);
+    ASSERT_NE(json, nullptr);
+
+    EXPECT_EQ(cJSON_GetObjectItem(json, "devAddr"), nullptr);
+    EXPECT_EQ(cJSON_GetObjectItem(json, "baseClass"), nullptr);
+    EXPECT_EQ(cJSON_GetObjectItem(json, "iManufacturer"), nullptr);
+    EXPECT_EQ(cJSON_GetObjectItem(json, "iProduct"), nullptr);
+    EXPECT_EQ(cJSON_GetObjectItem(json, "iSerialNumber"), nullptr);
+    EXPECT_EQ(cJSON_GetObjectItem(json, "bMaxPacketSize0"), nullptr);
+    EXPECT_EQ(cJSON_GetObjectItem(json, "bcdUSB"), nullptr);
+    EXPECT_EQ(cJSON_GetObjectItem(json, "bcdDevice"), nullptr);
+    EXPECT_EQ(cJSON_GetObjectItem(json, "authorizeStatus"), nullptr);
+
+    cJSON_Delete(json);
+}
+
+HWTEST_F(UsbCliSerializationTest, SerializeAccessory_AllFields, TestSize.Level1)
+{
+    USBAccessory accessory("mfg", "prod", "desc", "1.0", "SN001");
+
+    cJSON *json = SerializeAccessory(accessory);
+    ASSERT_NE(json, nullptr);
+
+    cJSON *item = cJSON_GetObjectItem(json, "manufacturer");
+    ASSERT_NE(item, nullptr);
+    EXPECT_STREQ(item->valuestring, "mfg");
+
+    item = cJSON_GetObjectItem(json, "product");
+    ASSERT_NE(item, nullptr);
+    EXPECT_STREQ(item->valuestring, "prod");
+
+    item = cJSON_GetObjectItem(json, "description");
+    ASSERT_NE(item, nullptr);
+    EXPECT_STREQ(item->valuestring, "desc");
+
+    item = cJSON_GetObjectItem(json, "version");
+    ASSERT_NE(item, nullptr);
+    EXPECT_STREQ(item->valuestring, "1.0");
+
+    item = cJSON_GetObjectItem(json, "serialNumber");
+    ASSERT_NE(item, nullptr);
+    EXPECT_STREQ(item->valuestring, "SN001");
+
+    cJSON_Delete(json);
+}
+
+HWTEST_F(UsbCliSerializationTest, SerializeSerialPort_PublicApiFields, TestSize.Level1)
+{
+    UsbSerialPort port;
+    port.portId_ = 3;
+    port.busNum_ = 5;
+    port.devAddr_ = 7;
+
+    cJSON *json = SerializeSerialPort(port);
+    ASSERT_NE(json, nullptr);
+
+    cJSON *item = cJSON_GetObjectItem(json, "portId");
+    ASSERT_NE(item, nullptr);
+    EXPECT_EQ(item->valueint, 3);
+
+    item = cJSON_GetObjectItem(json, "deviceName");
+    ASSERT_NE(item, nullptr);
+    EXPECT_STREQ(item->valuestring, "5-7");
+
+    EXPECT_EQ(cJSON_GetObjectItem(json, "busNum"), nullptr);
+    EXPECT_EQ(cJSON_GetObjectItem(json, "devAddr"), nullptr);
+    EXPECT_EQ(cJSON_GetObjectItem(json, "vid"), nullptr);
+    EXPECT_EQ(cJSON_GetObjectItem(json, "pid"), nullptr);
+    EXPECT_EQ(cJSON_GetObjectItem(json, "serialNum"), nullptr);
+
+    cJSON_Delete(json);
+}
+
+HWTEST_F(UsbCliSerializationTest, FormatSuccess_EmptyList, TestSize.Level1)
+{
+    std::vector<cJSON*> items;
+    std::string result = FormatSuccess(items);
+
+    cJSON *root = cJSON_Parse(result.c_str());
+    ASSERT_NE(root, nullptr);
+
+    cJSON *number = cJSON_GetObjectItem(root, "number");
+    ASSERT_NE(number, nullptr);
+    EXPECT_EQ(number->valueint, 0);
+
+    cJSON *list = cJSON_GetObjectItem(root, "list");
+    ASSERT_NE(list, nullptr);
+    EXPECT_EQ(cJSON_GetArraySize(list), 0);
+
+    cJSON_Delete(root);
+}
+
+HWTEST_F(UsbCliSerializationTest, FormatSuccess_WithItems, TestSize.Level1)
+{
+    USBAccessory acc1("mfg1", "prod1", "desc1", "1.0", "SN1");
+    USBAccessory acc2("mfg2", "prod2", "desc2", "2.0", "SN2");
+
+    std::vector<cJSON*> items;
+    items.push_back(SerializeAccessory(acc1));
+    items.push_back(SerializeAccessory(acc2));
+
+    std::string result = FormatSuccess(items);
+
+    cJSON *root = cJSON_Parse(result.c_str());
+    ASSERT_NE(root, nullptr);
+
+    cJSON *number = cJSON_GetObjectItem(root, "number");
+    ASSERT_NE(number, nullptr);
+    EXPECT_EQ(number->valueint, 2);
+
+    cJSON *list = cJSON_GetObjectItem(root, "list");
+    ASSERT_NE(list, nullptr);
+    EXPECT_EQ(cJSON_GetArraySize(list), 2);
+
+    cJSON_Delete(root);
+    for (auto *item : items) {
+        cJSON_Delete(item);
+    }
+}
+
+HWTEST_F(UsbCliSerializationTest, FormatError_Structure, TestSize.Level1)
+{
+    std::string result = FormatError(42, "test error");
+
+    cJSON *root = cJSON_Parse(result.c_str());
+    ASSERT_NE(root, nullptr);
+
+    cJSON *code = cJSON_GetObjectItem(root, "code");
+    ASSERT_NE(code, nullptr);
+    EXPECT_EQ(code->valueint, 42);
+
+    cJSON *message = cJSON_GetObjectItem(root, "message");
+    ASSERT_NE(message, nullptr);
+    EXPECT_STREQ(message->valuestring, "test error");
+
+    cJSON_Delete(root);
+}
